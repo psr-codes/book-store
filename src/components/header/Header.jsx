b4255c29c6c1985dd7f6744424f2d79c2208d8a0
Lyt Code <rdilshad3559@gmail.com>
2024-09-26 14:55:37 +0530

    npm init

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Topbar from "./topbar";
+import Navbar from "./Navbar";
+import TopbarStrip from "./TopbarStrip";
+
+const Header = () => {
+  return (
+    <div>
+      {/* <TopbarStrip /> */}
+      <Topbar />
+      {/* <Navbar /> */}
+    </div>
+  );
+};
+
+export default Header;
