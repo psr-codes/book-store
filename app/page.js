5ca87d1d82aaf6ed0031f9724c4f0d0e100fa56a
Lyt Code <rdilshad3559@gmail.com>
2024-10-02 20:54:20 +0530

    products page is done

--- app/page.js
+++ app/page.js
@@ -2,16 +2,16 @@ import Image from "next/image";
 import localFont from "next/font/local";
 import LandingPage from "@/components/landing/LandingPage";
 
-const geistSans = localFont({
-    src: "./fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
-});
-const geistMono = localFont({
-    src: "./fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
-});
+// const geistSans = localFont({
+//     src: "./fonts/GeistVF.woff",
+//     variable: "--font-geist-sans",
+//     weight: "100 900",
+// });
+// const geistMono = localFont({
+//     src: "./fonts/GeistMonoVF.woff",
+//     variable: "--font-geist-mono",
+//     weight: "100 900",
+// });
 
 export default function Home() {
     return (
