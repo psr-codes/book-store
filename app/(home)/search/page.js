8157bc6ea7a79a65ca8588d2d8f9b12726de3147
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2024-10-17 21:26:53 +0530

    added suspense boudary in search

--- app/(home)/search/page.js
+++ app/(home)/search/page.js
@@ -1,13 +1,19 @@
 "use client";
-import React from "react";
-
+import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
-const Page = () => {
+
+const SearchContent = () => {
     const searchParams = useSearchParams();
     const query = searchParams.get("query");
 
     console.log("query is", query);
     return <div>query: {query}</div>;
 };
+
+const Page = () => (
+    <Suspense fallback={<div>Loading...</div>}>
+        <SearchContent />
+    </Suspense>
+);
 
 export default Page;
